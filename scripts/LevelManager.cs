46f5a5f61a423f0f86ff947d2724ba90419a3bb4
Mehmet Emre Kayacan <116388836+mehmetemrekayacan@users.noreply.github.com>
2024-01-03 12:04:18 +0300

    Update LevelManager.cs

--- scripts/LevelManager.cs
+++ scripts/LevelManager.cs
@@ -3,13 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    // Bu metot, butona týklandýðýnda çaðrýlacak
+    
     public void LoadLevel(int levelNumber)
     {
-        // levelNumber parametresi, butona týklandýðýnda geçilecek seviyenin numarasýný belirtir
+        
         string levelName = "Level_" + levelNumber;
 
-        // Belirtilen seviyeye geçiþ yap
+        
         SceneManager.LoadScene(levelName);
     }
 }
